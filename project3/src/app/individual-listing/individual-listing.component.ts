9bc0f7f99a7c0f93353f5d7d002be06b4ad4631d
willsorg <willsorg@udel.edu>
2017-12-07 19:38:59 -0500

    Final Push

--- project3/src/app/individual-listing/individual-listing.component.ts
+++ project3/src/app/individual-listing/individual-listing.component.ts
@@ -16,7 +16,12 @@ export class IndividualListingComponent implements OnInit {
 	apiSvc: any;
 	properties : any;
   property: Property;
-  apiKey : string = "AIzaSyAw_H4lE8TiLAl8sNhvbtfaQ5LlODwYAxc"
+  apiKey : string = "AIzaSyAw_H4lE8TiLAl8sNhvbtfaQ5LlODwYAxc";
+  rent : string = "Not Set";
+  bedrooms: string = "Not Set";
+  bathrooms: string = "Not Set";
+  tenants: string = "Not Set";
+  
 
   link: string = "https://maps.googleapis.com/maps/api/streetview?size=450x300&location=" + this.route.snapshot.params['id'] + "&key=" + this.apiKey;
 
@@ -40,7 +45,37 @@ export class IndividualListingComponent implements OnInit {
         console.log(res);
         this.properties = res;
         console.log(this.properties);
-        this.property = this.properties[0];
+        
+        //console.log(property);
+        
+        if(this.properties.rent == 0){
+          this.rent = "Contact Owner for Details";
+        }
+        else{
+          this.rent = "$"+this.properties.rent;
+        }
+        if(this.properties.bathrooms == 0){
+          this.bathrooms = "Info Not Available/ Multiple Options Available";
+    
+        }
+        else{
+          this.bathrooms = ""+this.properties.bathrooms;
+        }
+        if(this.properties.bedrooms == 0){
+          this.bedrooms = "Info Not Available/ Multiple Options Available";
+    
+        }
+        else{
+          this.bedrooms = ""+this.properties.bedrooms;
+        }
+        if(this.properties.tenants == 0){
+          this.tenants= "Info Not Available/ Multiple Options Available";
+    
+        }
+        else{
+          this.tenants = ""+this.properties.tenants;
+        }
+        
       },
       err => {
         console.log("Error occured");
@@ -58,6 +93,10 @@ export class IndividualListingComponent implements OnInit {
   findAddressURL() {
     let url = "https://maps.googleapis.com/maps/api/streetview?size=450x300&location=" + this.properties.address + ", Newark DE&key=" + this.apiKey;
     return url;
+  }
+
+  fixMissingInfo(){
+    
   }
 
 }
