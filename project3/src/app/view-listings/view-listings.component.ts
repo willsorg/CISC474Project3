b5b6946ed7a0a0498c03750b56025db7243f051e
willsorg <willsorg@udel.edu>
2017-12-11 19:24:13 -0500

    AWS second push

--- project3/src/app/view-listings/view-listings.component.ts
+++ project3/src/app/view-listings/view-listings.component.ts
@@ -18,7 +18,7 @@ export class ViewListingsComponent implements OnInit {
     }
 
   ngOnInit() {
-  	this.http.get('http://localhost:3000/api/all').subscribe(data => {
+  	this.http.get('http://ec2-34-200-119-236.compute-1.amazonaws.com:3000/api/all').subscribe(data => {
       console.log(data);
       this.properties = data;
       //this.properties = [{"id":"1","address":"14 Annabelle St","rent":"500","bedrooms":"4","bathrooms":"1","tenants":"4"},
